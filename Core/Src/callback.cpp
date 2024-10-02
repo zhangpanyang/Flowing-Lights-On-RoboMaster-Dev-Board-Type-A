008900bdf8686ef280ff1ddb4de2b9592e5176f6
zhangpanyang <azhangpanyang@126.com>
2024-10-02 15:41:13 +0800

    Changes

---
+++ Core/Src/callback.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Sunny on 24-10-2.
+//
+
+#include "main.h"
+#include "tim.h"
+
+int count = 0;
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	if (htim == &htim1)
+	{
+		count++;
+		if (count == 1000)
+		{
+			HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
+			count = 0;
+		}
+
+	}
+}
