b12225e41324af482cfdca165e9777633198972a
zhangpanyang <azhangpanyang@126.com>
2024-10-02 18:15:53 +0800

    Turned gamma to 2.2

--- Core/Src/start.cpp
+++ Core/Src/start.cpp
@@ -11,14 +11,14 @@ LED leds[8];
 
 void initLEDS()
 {
-	leds[0] = LED(LED_1_GPIO_Port, LED_1_Pin, 128, 1);
-	leds[1] = LED(LED_2_GPIO_Port, LED_2_Pin, 128, 1);
-	leds[2] = LED(LED_3_GPIO_Port, LED_3_Pin, 128, 1);
-	leds[3] = LED(LED_4_GPIO_Port, LED_4_Pin, 128, 1);
-	leds[4] = LED(LED_5_GPIO_Port, LED_5_Pin, 128, 1);
-	leds[5] = LED(LED_6_GPIO_Port, LED_6_Pin, 128, 1);
-	leds[6] = LED(LED_7_GPIO_Port, LED_7_Pin, 128, 1);
-	leds[7] = LED(LED_8_GPIO_Port, LED_8_Pin, 128, 1);
+	leds[0] = LED(LED_1_GPIO_Port, LED_1_Pin, 128, 2.2);
+	leds[1] = LED(LED_2_GPIO_Port, LED_2_Pin, 128, 2.2);
+	leds[2] = LED(LED_3_GPIO_Port, LED_3_Pin, 128, 2.2);
+	leds[3] = LED(LED_4_GPIO_Port, LED_4_Pin, 128, 2.2);
+	leds[4] = LED(LED_5_GPIO_Port, LED_5_Pin, 128, 2.2);
+	leds[5] = LED(LED_6_GPIO_Port, LED_6_Pin, 128, 2.2);
+	leds[6] = LED(LED_7_GPIO_Port, LED_7_Pin, 128, 2.2);
+	leds[7] = LED(LED_8_GPIO_Port, LED_8_Pin, 128, 2.2);
 
 	for (int i = 0; i < 8; i++)
 	{
